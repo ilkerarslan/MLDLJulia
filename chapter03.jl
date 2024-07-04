2010708f66bcd2e5291c14f20196589219d36c3c
ilkerarslan <ilker.arslan@outlook.com>
2024-07-04 23:37:11 +0300

    chapter03

--- chapter03.jl
+++ chapter03.jl
@@ -5,7 +5,7 @@ using Random, Plots, StatsBase
 using Nova
 using Nova.ModelSelection: train_test_split
 using Nova.PreProcessing: StandardScaler
-using Nova.LinearModel: Perceptron, MultiClassPerceptron
+using Nova.LinearModel: MulticlassPerceptron, LogisticRegression
 using Nova.Metrics: accuracy_score
 
 # Data
@@ -32,118 +32,7 @@ stdscaler(Xtrn)
 Xtrn_std = stdscaler(Xtrn)
 Xtst_std = stdscaler(Xtst)
 
-
-abstract type AbstractModel end
-
-# Activation functions
-"""Linear activation function"""
-linearactivation(X) = X
-
-# Common methods
-"""Calculate net input"""
-net_input(m::AbstractModel, x::AbstractVector) = m.W' * x .+ m.b
-net_input(m::AbstractModel, X::AbstractMatrix) = X * m.W .+ m.b'
-
-"""AbstractModel batch prediction"""
-(m::AbstractModel)(X::AbstractMatrix) = [m(x) for x in eachrow(X)]
-
 # Multiclass Perceptron
-mutable struct MulticlassPerceptron <: AbstractModel
-    # Parameters
-    W::Matrix{Float64}
-    b::Vector{Float64}
-    losses::Vector{Float64}
-    fitted::Bool
-    classes::Vector{Any}
-
-    # Hyper parameters
-    η::Float64
-    num_iter::Int
-    random_state::Union{Nothing, Int64}
-    optim_alg::Symbol
-    batch_size::Int 
-end
-
-function MulticlassPerceptron(; η=0.01, num_iter=100, random_state=nothing,
-            optim_alg=:SGD, batch_size=32)
-    if !(optim_alg ∈ [:SGD, :Batch, :MiniBatch])
-        throw("""`optim_alg` should be in [:SGD, :Batch, :MiniBatch]""")
-    else        
-        return MulticlassPerceptron(Matrix{Float64}(undef, 0, 0), Float64[], Float64[], false, [], η, num_iter, random_state, optim_alg, batch_size)
-    end
-end
-
-# Model training
-"""Multiclass Perceptron training model"""
-function (m::MulticlassPerceptron)(X::Matrix, y::Vector)
-    if m.random_state !== nothing
-        Random.seed!(m.random_state)
-    end
-    empty!(m.losses)
-
-    # Get unique classes
-    m.classes = sort(unique(y))
-    n_classes = length(m.classes)
-    n_features = size(X, 2)
-
-    # Initialize weights and bias
-    m.W = randn(n_features, n_classes) ./ 100
-    m.b = zeros(n_classes)
-
-    # Create a dictionary to map classes to indices
-    class_to_index = Dict(class => i for (i, class) in enumerate(m.classes))
-
-    if m.optim_alg == :SGD
-        n = length(y)
-        for _ in 1:m.num_iter
-            error = 0
-            for i in 1:n
-                xi, yi = X[i, :], y[i]
-                ŷ_scores = net_input(m, xi)
-                ŷ_index = argmax(ŷ_scores)
-                yi_index = class_to_index[yi]
-                
-                if ŷ_index != yi_index
-                    error += 1
-                    # Update weights and bias for the correct class
-                    m.W[:, yi_index] .+= m.η * xi
-                    m.b[yi_index] += m.η
-                    # Update weights and bias for the predicted class
-                    m.W[:, ŷ_index] .-= m.η * xi
-                    m.b[ŷ_index] -= m.η
-                end
-            end
-            push!(m.losses, error)
-        end
-        m.fitted = true
-    elseif m.optim_alg == :Batch
-        # Implement batch gradient descent
-    elseif m.optim_alg == :MiniBatch
-        # Implement mini-batch gradient descent
-    end
-end
-
-"""Multiclass Perceptron prediction with single observation"""
-function (m::MulticlassPerceptron)(x::AbstractVector)
-    if !m.fitted
-        throw(ErrorException("Model is not fitted yet."))
-    end
-    scores = net_input(m, x)
-    class_index = argmax(scores)
-    return m.classes[class_index]
-end
-
-"""Multiclass Perceptron prediction with multiple observations"""
-function (m::MulticlassPerceptron)(X::AbstractMatrix)
-    if !m.fitted
-        throw(ErrorException("Model is not fitted yet."))
-    end
-    scores = net_input(m, X)
-    class_indices = [argmax(score) for score in eachrow(scores)]
-    return [m.classes[i] for i in class_indices]
-end
-
-
 mcp = MulticlassPerceptron(η=0.1, random_state=1)
 mcp(Xtrn_std, ytrn)
 ŷ = mcp(Xtst_std)
@@ -164,69 +53,16 @@ end
 Xcomb_std = vcat(Xtrn_std, Xtst_std)
 ycomb = vcat(ytrn, ytst)
 
-plot_decision_region(ppn, Xcomb_std, ycomb)
-
-using MLJ, MLJModels, MLJLinearModels
-using Distances, Optim
-# Load data
-iris = MLJ.load_iris()
-MLJ.selectrows(iris, 1:3) |> MLJ.pretty
-MLJ.schema(iris)
-iris = DataFrames.DataFrame(iris)
-y, X = MLJ.unpack(iris, ==(:target))
-X = X[:, 3:4]
-MLJ.first(X, 3) |> MLJ.pretty
-
-Random.seed!(1)
-train, test = MLJ.partition(eachindex(y), 0.7, stratify=y)
-Xtrn, Xtst = X[train, :], X[test, :]
-ytrn, ytst = y[train], y[test]
-
-println("Labels count in y: ", values(StatsBase.countmap(y)))
-println("Labels counts in ytrn: ", values(StatsBase.countmap(ytrn)))
-println("Labels counts in ytst: ", values(StatsBase.countmap(ytst)))
-
-# Define the StandardScaler equivalent
-standardizer = MLJ.Standardizer()
-# Fit the standardizer to the training data
-mach = MLJ.machine(standardizer, Xtrn)
-MLJ.fit!(mach)
-# Transform both training and test data
-Xtrn_std = MLJ.transform(mach, Xtrn)
-Xtst_std = MLJ.transform(mach, Xtst)
-
-# Search for Perceptron
-models(matching(X, y))
-models("Perceptron")
-
-doc("KernelPerceptronClassifier", pkg="BetaML")
-Model = @load KernelPerceptronClassifier pkg=BetaML
-pcl = Model()
-mach = machine(pcl, Xtrn_std, ytrn);
-MLJ.fit!(mach)
-
-ŷ = MLJ.predict(mach, Xtst_std)
-
-print("Misclassified examples: $(sum(ytst .!= mode.(ŷ)))")
-MLJ.accuracy(ytst, MLJ.mode.(ŷ))
-evaluate!(mach, resampling=Holdout(fraction_train=0.7),
-         measures=[log_loss, accuracy],
-         verbosity=0)
-
-X = vcat(Xtrn_std, Xtst_std)
-y = vcat(ytrn, ytst)
-plot_decision_regions(X, y, mach, test_idx=105:150, length=500)
+plot_decision_region(mcp, Xcomb_std, ycomb)
 
 # Logistic Regression
 
-function sigmoid(z)
-    return 1.0 ./ (1.0 .+ exp.(-z))
-end
-
-z = range(-7, 7, step=0.1)
-sigma_z = sigmoid(z)
-
 begin
+    function sigmoid(z)
+        return 1.0 ./ (1.0 .+ exp.(-z))
+    end
+    z = range(-7, 7, step=0.1)
+    sigma_z = sigmoid(z)    
     plot(z, sigma_z, label="σ(z)", legend=false)
     vline!([0.0], color=:black)
     ylims!(-0.1, 1.1)
@@ -237,28 +73,36 @@ begin
     plot!(size=(600, 400), margin=3Plots.mm)        
 end
 
-loss_y(z, y) = y==1 ? -log(sigmoid(z)) : -log(1-sigmoid(z))
+begin
+    loss_y(z, y) = y==1 ? -log(sigmoid(z)) : -log(1-sigmoid(z))
 
-z = -10:0.1:10;
-sigma_z = sigmoid(z)
-c0 = [loss_y(x, 0) for x in z]
-c1 = [loss_y(x, 1) for x in z]
+    z = -10:0.1:10;
+    sigma_z = sigmoid(z)
+    c0 = [loss_y(x, 0) for x in z]
+    c1 = [loss_y(x, 1) for x in z]
+    
+    plot(sigma_z, [c0, c1],
+         label=["L(w,b) if y=1" "L(w,b) if y=0"],
+         ylims=(0.0, 5.1),
+         legend=:top,
+         linestyle=[:solid :dash],
+         linewidth=2,
+         xlabel="σ(z)",
+         ylabel="L(w,b)")    
+end
 
-plot(sigma_z, [c0, c1],
-     label=["L(w,b) if y=1" "L(w,b) if y=0"],
-     ylims=(0.0, 5.1),
-     legend=:top,
-     linestyle=[:solid :dash],
-     linewidth=2,
-     xlabel="σ(z)",
-     ylabel="L(w,b)")
 
 idx = (ytrn .== "setosa") .|| (ytrn .== "versicolor")
 Xtrn01_subset = Xtrn_std[idx, :] |> Matrix
 ytrn01_subset = ytrn[idx]
 ytrn01_subset = [x == "setosa" ? 0 : 1 for x in ytrn01_subset]
 
-lrgd = LogisticRegression()
+lrgd = LogisticRegression(η=0.3, num_iter=1000, random_state=1, optim_alg=:Batch)
+lrgd(Xtrn01_subset, ytrn01_subset)
+
+
+
+
 fitmodel!(lrgd, Xtrn01_subset, ytrn01_subset,
           η=0.3, num_iter=1000, seed=1)
 
